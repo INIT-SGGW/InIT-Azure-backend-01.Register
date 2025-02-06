1896f40e65cabc721c9970f8b039010955e39628
Krzysztof Rozbicki <krzysz.rozbicki@gmail.com>
2025-02-07 00:50:01 +0100

    Add register User endpoint

--- src/server/register-server.go
+++ src/server/register-server.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"INIT-SGGW/InIT-Azure-backend-01.Register/config"
+	"INIT-SGGW/InIT-Azure-backend-01.Register/handler"
 	"INIT-SGGW/InIT-Azure-backend-01.Register/initializer"
-	"context"
+	"INIT-SGGW/InIT-Azure-backend-01.Register/model"
+	"INIT-SGGW/InIT-Azure-backend-01.Register/repository"
 	"fmt"
+
+	"go.uber.org/zap"
+
+	"context"
 	"net/http"
 
 	"github.com/danielgtaylor/huma/v2"
@@ -11,37 +18,52 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-type HealthProbeResponse struct {
-	Body struct {
-		Status string `json:"status" example:"I'm Alive!" doc:"Health check"`
-	}
-}
-
 func main() {
-	logger := initializer.CreateLogger()
-	fmt.Println("Hello InIT!")
+	config := config.NewEnvConfig()
+	logger := initializer.CreateLogger(config.LogPath)
+	defer logger.Sync()
+
+	logger.Info("Starting service",
+		zap.String("Environment", config.Env))
+
+	repo := repository.NewMongoRepository(config.DbConnStr, config.DbName, logger)
+	registerHandler := handler.NewRegisterHandler(logger, repo)
+
 	r := chi.NewRouter()
 	r.Use(initializer.New(logger))
 
 	standardApiRouter := chi.NewRouter()
-
 	r.Mount("/v1/api", standardApiRouter)
 
-	api := humachi.New(r, huma.DefaultConfig("KN INIT Website API", "1.0.0"))
+	api := createHumaApi("KN INIT Website Register API", "1.0.0", r)
+	addRoutes(api, *registerHandler)
 
-	huma.Get(api, "/hearthbeat", func(ctx context.Context, input *struct{}) (*HealthProbeResponse, error) {
-		resp := &HealthProbeResponse{}
+	http.ListenAndServe(fmt.Sprintf(":%s", config.ListenPort), r)
+}
+
+func createHumaApi(title, version string, r chi.Router) huma.API {
+	humaConfig := huma.DefaultConfig(title, version)
+	humaConfig.DocsPath = "/api/v1/register/docs"
+	humaConfig.OpenAPIPath = "/api/v1/register/openapi"
+	humaConfig.SchemasPath = "/api/v1/register/schemas"
+
+	api := humachi.New(r, humaConfig)
+	return api
+}
+
+func addRoutes(api huma.API, handler handler.RegisterHandler) {
+
+	huma.Get(api, "/hearthbeat", func(ctx context.Context, input *struct{}) (*model.HealthProbeResponse, error) {
+		resp := &model.HealthProbeResponse{}
 		resp.Body.Status = "I'm Alive!"
 		return resp, nil
 	})
 
-	// huma.Register(api, huma.Operation{
-	// 	OperationID: "register-user",
-	// 	Method:      http.MethodPost,
-	// 	Path:        "/api/v1/register/user",
-	// 	Summary:     "Register user",
-	// 	Description: "Register user and send confirmation email to provided adress with unique token for account verification",
-	// }, registerServ.HandleRegisterUserRequest)
-
-	http.ListenAndServe(":3131", r)
+	huma.Register(api, huma.Operation{
+		OperationID: "register-user",
+		Method:      http.MethodPost,
+		Path:        "/api/v1/register/user",
+		Summary:     "Register user",
+		Description: "Register user and send confirmation email to provided adress with unique token for account verification",
+	}, handler.HandleRegisterUserRequest)
 }
